78a19fc54b998c275a1c361d16f48aa66673c870
Abdul Kader Rabbani <rabbanictgbd@gmail.com>
2025-02-24 18:14:32 +0600

    25.5

--- test.js
+++ test.js
@@ -4,3 +4,12 @@ console.log("Hey")
 }else{
 console.log("Hi")
 }
+
+const numbers=[1,2,5,7,10]
+let sum=0
+for(let num of numbers){
+    sum++
+    console.log(num)
+} return sum
+
+console.log(sum)
